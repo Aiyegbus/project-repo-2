40edd57bb2a3f4d3732be2428ddaa9c79b836463
Aiyegbus <133551804+Aiyegbus@users.noreply.github.com>
2023-10-25 00:15:02 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -54,12 +54,12 @@ resource "aws_route" "route_to_igw" {
   gateway_id             = aws_internet_gateway.my_igw.id
 }
 
-resource "aws_subnet_route_table_association" "subnet_1_association" {
+resource "aws_route_table_association" "subnet_1_association" {
   subnet_id      = aws_subnet.my_subnet_1.id
   route_table_id = aws_route_table.my_route_table.id
 }
 
-resource "aws_subnet_route_table_association" "subnet_2_association" {
+resource "aws_route_table_association" "subnet_2_association" {
   subnet_id      = aws_subnet.my_subnet_2.id
   route_table_id = aws_route_table.my_route_table.id
 }
