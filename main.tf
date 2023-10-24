2847675ae602db46814070906a8cc5e7f58af7f4
Aiyegbus <133551804+Aiyegbus@users.noreply.github.com>
2023-10-25 00:11:59 +0100

    more modifications

--- main.tf
+++ main.tf
@@ -20,6 +20,10 @@ resource "aws_vpc" "my_vpc" {
   }
 }
 
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
 resource "aws_subnet" "my_subnet_1" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.0.0/24"
@@ -40,6 +44,25 @@ resource "aws_subnet" "my_subnet_2" {
   }
 }
 
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+resource "aws_route" "route_to_igw" {
+  route_table_id         = aws_route_table.my_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.my_igw.id
+}
+
+resource "aws_subnet_route_table_association" "subnet_1_association" {
+  subnet_id      = aws_subnet.my_subnet_1.id
+  route_table_id = aws_route_table.my_route_table.id
+}
+
+resource "aws_subnet_route_table_association" "subnet_2_association" {
+  subnet_id      = aws_subnet.my_subnet_2.id
+  route_table_id = aws_route_table.my_route_table.id
+}
 resource "aws_security_group" "my_sg" {
   name        = "my-security-group"
   description = "My security group"
